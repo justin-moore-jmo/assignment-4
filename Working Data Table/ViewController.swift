f9981c97666927e276ee3b86525049674234aac4
justin moore <justin.moore.jmo@gmail.com>
2019-04-04 03:22:30 -0500

    Final Project

--- Working Data Table/ViewController.swift
+++ Working Data Table/ViewController.swift
@@ -8,13 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    //this sets number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selectedArray.count
+    }
+    
+    //this fills rows with content
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "cellInTableView")!
+        let holdertText = selectedArray[indexPath.row]
+        myCell.textLabel?.text = holdertText
+        return myCell
+    }
+    
+    var myCoolData:[String] = ["The Shawshank Redemption", "Pulp Fiction" , "Fight Club", "Spider-Man: Into the Spider-Verse"]
+    var mySweetData:[String] = ["To Kill a Mockingbird", "Things Fall Apart" , "The Kite Runner" , "Temeraire Series"]
 
+    @IBOutlet weak var myCustomTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myCustomTableView.dataSource = self
+        selectedArray = myCoolData
     }
-
-
+    
+    
+    @IBAction func removeButton(_ sender: Any) {
+        if myCoolData.count > 0{
+        myCoolData.removeLast()
+        }
+        selectedArray = myCoolData
+        myCustomTableView.reloadData()
+    }
+    
+    @IBAction func removeButton2(_ sender: Any) {
+        if mySweetData.count > 0{
+        mySweetData.removeLast()
+        }
+        selectedArray = mySweetData
+        myCustomTableView.reloadData()
+    }
+    
+    
+    @IBOutlet weak var appendMovieTextField: UITextField!
+    
+    @IBAction func addButton(_ sender: Any) {
+        myCoolData.append(appendMovieTextField.text ?? "Blah")
+        selectedArray = myCoolData
+        myCustomTableView.reloadData()
+    }
+    
+    @IBAction func addButton2(_ sender: Any) {
+    mySweetData.append(appendMovieTextField.text ?? "Blah")
+        selectedArray = mySweetData
+        myCustomTableView.reloadData()
+    }
+    
+    
+    var selectedArray:[String] = []
+    
+    @IBAction func switchToMovies(_ sender: Any) {
+        selectedArray = myCoolData
+        myCustomTableView.reloadData()
+    }
+    
+    
+    @IBAction func switchToBooks(_ sender: Any) {
+        selectedArray = mySweetData
+        myCustomTableView.reloadData()
+    }
 }
 
